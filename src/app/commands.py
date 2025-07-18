d96f70c603dfbbccbc22efa19eddee595cdd1f93
Christian Martinez <cgmartinez11142@gmail.com>
2025-07-18 13:52:09 -0700

    add more detailed docstrings for help command

--- src/app/commands.py
+++ src/app/commands.py
@@ -1,8 +1,11 @@
 """
-Add gear query logic
+Logic for CLI commands.
 """
 
 from datetime import datetime
+
+import typer
+from typing_extensions import Annotated
 
 from data.fetch import fetch_gear_list
 from data.post import post_gear_list, post_gear_matches, post_gear_query
@@ -11,14 +14,18 @@ from scrapers.gear_scraper import get_latest_gear
 from .gear import GearQuery, get_active_queries
 
 
-def add_gear_query(search_term: str):
-    """Create a new gear query"""
+def add_gear_query(search_term: Annotated[str, typer.Argument()]):
+    """
+    Search for a new item with SEARCHTERM.
+    """
     timestamp = datetime.now().strftime("%Y-%m-%d")
     new_gear_query = GearQuery(search_term, timestamp)
     post_gear_query(new_gear_query.search_term, new_gear_query.timestamp)
 
 def update_gear_matches():
-    """Checks current gear list for matches for any active queries"""
+    """
+    Search for matches for all open queries. Update results.
+    """
     gear_list = fetch_gear_list()
     active_queries = get_active_queries()
     for query in active_queries:
@@ -27,6 +34,8 @@ def update_gear_matches():
         post_gear_matches(matches)
 
 def update_gear_list():
-    """Scrapes latest gear at Hank's and updates DB"""
+    """
+    Run web scraper to update gear list.
+    """
     gear_list = get_latest_gear()
     post_gear_list(gear_list)
--- src/cli.py
+++ src/cli.py
@@ -3,23 +3,16 @@ CLI app
 """
 
 import typer
-from typing_extensions import Annotated
 
-from app.app import add_gear_query, update_gear_list, update_gear_matches
+from app.commands import add_gear_query, update_gear_list, update_gear_matches
 from data.data import init_db
 
 app = typer.Typer()
 
-@app.command()
-def setup():
-    init_db()
-
-@app.command()
-def main(search_term: Annotated[str, typer.Argument()]):
-    add_gear_query(search_term)
-
-app.command()(update_gear_matches)
-app.command()(update_gear_list)
+app.command("setup")(init_db)
+app.command("search")(add_gear_query)
+app.command("update-matches")(update_gear_matches)
+app.command("update-gear")(update_gear_list)
 
 if __name__ == "__main__":
     app()
