12c362dd936e729d817a1ebc8c38d5d8815ebdee
Christian Martinez <cgmartinez11142@gmail.com>
2025-08-02 20:42:25 -0700

    add inital discord bot functions

---
+++ src/app/discord_notification.py
@@ -0,0 +1,41 @@
+"""
+Logic for discord notification for gear matches
+"""
+import os
+
+import discord
+from discord import app_commands
+
+guild_id = os.environ.get("GUILD_ID")
+token = os.environ.get("DISCORD_TOKEN")
+my_guild = discord.Object(id=guild_id)
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=my_guild)
+        await self.tree.sync(guild=my_guild)
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+@client.tree.command()
+async def send_message(interaction: discord.Interaction):
+    """send a message with matches"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
